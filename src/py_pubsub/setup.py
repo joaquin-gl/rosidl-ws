2e70927a6d61aea01833a18b8e72c022a8058315
Joaquin Giraldo-Laguna <joaquin@multiplylabs.com>
2021-03-10 17:55:02 -0800

    first commit

---
+++ src/py_pubsub/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'py_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='joaquin',
+    maintainer_email='joaquin@multiplylabs.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_pubsub.publisher_member_function:main',
+            'listener = py_pubsub.subscriber_member_function:main',
+            'talkercustom = py_pubsub.publisher_member_function_custom:main',
+            'listenercustom = py_pubsub.subscriber_member_function_custom:main',
+        ],
+    },
+)
